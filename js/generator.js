bf14f4f461377af7a8101a111ac00596a4dfa2d7
Gautham Sajith <gsajith@umich.edu>
2018-05-17 12:08:03 -0500

    Add phrases to JS file

--- js/generator.js
+++ js/generator.js
@@ -1,1 +1,8 @@
-alert(0);
+var VILLAINS = ["Postmodern Neomarxists", "Feminists (who secretly crave domination)", "Leftist academics", "Dangerous ideologues", "Derrida and Foucault", "Indoctrinated students", "Social justice types", "Radical trans activists", "Politically correct HR departments", "Actual Communists", "The left", "Millenials with a victimhood mentality"]
+var SPICY_VERB_PHRASE = ["are totally corrupting", "have zero respect for", "want to annihilate", "assault the archetype of", "don't bloody believe in", "will quickly infect", "unleash the Chaos Dragon of", "dismiss and transgress", "must be stopped from attacking", "will make Gulags out of", "feminize and weaken"]
+var FAVORITE_THINGS = ["the dominance hierarchy", "the metaphorical substrate", "Western values", "the classical humanities", "the individual", "the Hero's Journey", "the fabric of Being", "Solzhenitsyn's genius", "Carl Jung's legacy", "IQ testing's ability to determine achievement", "the divine Logos", "the inescapable tragedy and suffering of life", "the humble lobster's quest"]
+var BECAUSE_OF_THEIR = ["because of their"]
+var EVIL_WEAPONS = ["murderous equity doctrine", "dangerous egalitarian utopia", "hatred of Objective Truth", "compelled speech", "group identity", "Maoist pronouns", "propaganda from Frozen", "radical collectivism", "lens of power for everything", "disdain for Being", "ideological bill C-16", "low serotonin levels and poor posture", "totalitarian ideology which I've been studying for decades"]
+var OMINOUS_CONCLUSION = ["and we can't even have a conversation about it!", "so just ask the Kulaks how that worked out.", "and no one is talking about it!", "as you can bloody well imagine!", "just like Nietzsche prophesized.", "so you should sign up for the Self Authoring Suite.", "[while still ignoring the original question] so let me ask you this...", "and you can be damn sure about that!"]
+
+Alert(Math.floor(Math.random() * VILLAINS.length));
