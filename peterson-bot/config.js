fbddc2fec6ee76db78cac74e84ceeb0925b2e7df
Gautham Sajith <gsajith@umich.edu>
2018-05-18 14:58:27 -0500

    Fixed env variables

---
+++ peterson-bot/config.js
@@ -0,0 +1,15 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+require('dotenv').config()
+
+module.exports = {
+    consumer_key: process.env.CONSUMER_KEY,  
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,  
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+}
